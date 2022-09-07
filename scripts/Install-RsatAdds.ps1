584d21b59cbb84919a4d176ea5fb5b74cd07e811
BULENS Virgil <Virgil.BULENS@tucrail.be>
2022-09-07 11:51:11 +0200

    Install-RsatAdds

---
+++ scripts/Install-RsatAdds.ps1
@@ -0,0 +1,27 @@
+#
+# Variables
+#
+$ErrorActionPreference = "Stop"
+
+
+#
+# Main
+#
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\" `
+    -Name "UseWUServer" `
+    -Value 0
+
+Restart-Service -Name "wuauserv" `
+    -Force
+
+Get-WindowsCapability -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0" `
+    -Online | `
+    Add-WindowsCapability `
+    -Online
+
+Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\" `
+    -Name "UseWUServer" `
+    -Value 1
+
+Restart-Service -Name "wuauserv" `
+    -Force
