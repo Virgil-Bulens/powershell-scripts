192135d331b9d32b264dcbbb2572f2f5b126d789
BULENS Virgil <Virgil.BULENS@tucrail.be>
2022-09-07 10:57:58 +0200

    clean desktops

--- scripts/Start-StartupClean.ps1
+++ scripts/Start-StartupClean.ps1
@@ -14,3 +14,26 @@ Get-ChildItem -Path $DownloadFolder `
     -Force | `
     Remove-Item -Recurse `
     -Force
+
+# Get OneDrive folder
+$OneDriveFolder = Get-ChildItem -Path $env:USERPROFILE | `
+    Where-Object Name -Like "*OneDrive*"
+
+# Get Desktop folder
+$DesktopFolder = Get-ChildItem -Path $OneDriveFolder.FullName | `
+    Where-Object Name -EQ "Desktop"
+
+# Clean Desktop
+Get-ChildItem -Path $DesktopFolder.FullName `
+    -Recurse `
+    -Force | `
+    Where-Object Name -NE "FUCK OFF.lnk" | `
+    Remove-Item -Recurse `
+    -Force
+
+# Clean Public Desktop
+Get-ChildItem -Path "$env:PUBLIC\Desktop" `
+    -Recurse `
+    -Force | `
+    Remove-Item -Recurse `
+    -Force
